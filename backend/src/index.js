31bf71b2a985fbf5c933f3f63b9ba747d4d82636
lakshyraj5 <lakshyraj8chouhan@gmail.com>
2024-12-19 17:22:41 +0530

    loading screen done when user checkauth works and if user authenticate then take to home page if not take to login page

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,16 @@ import dotenv from "dotenv"
 import {connectDB} from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 import messageRoutes from "./models/message.model.js"
 dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 const PORT =process.env.PORT;
 app.use("/api/auth" , authRoutes);
 app.use("/api/message",messageRoutes);
