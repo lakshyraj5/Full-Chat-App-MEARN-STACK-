f0d25226e5235c04ebbae351a66ca3a2b9e92809
lakshyraj5 <lakshyraj8chouhan@gmail.com>
2024-12-31 12:57:47 +0530

    check

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -2,7 +2,7 @@
     return (
         <div className="hidden lg:flex items-center jusitfy-center bg-base-200 px-12">
             <div className="max-w-md text-center">
-                <div className="grid grid-cols-3 gap-3 mb-8">
+                <div className="grid grid-cols-3 gap-3 mb-8 ">
                     {[...Array(9)].map((_,i)=>(
                         <div
                         key={i}
@@ -10,7 +10,7 @@
                         />
                     ))}
                 </div>
-                <h2 className="text-2xl font-bold mb-4">{title}</h2>
+                <h2 className="text-2xl font-bold mb-4 ">{title}</h2>
                 <p className="text-base-content/60">{subtitle}</p>
             </div>
         </div>
