da8a538d17476d1d74ab4d9684e5a1e5991cf2e9
lakshyraj5 <lakshyraj8chouhan@gmail.com>
2025-01-02 17:44:35 +0530

    profile page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -36,6 +36,22 @@ export const useAuthStore = create((set)=>({
             set({isSigningUp:false});
         }
     },
+
+    
+    login: async (data)=>{
+        set({isLoggingIn:true});
+        try {
+            const res = await axiosInstance.post("/auth/login",data);
+            set({authUser:res.data});
+            toast.success("Logged in successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+        finally{
+            set({isLoggingIn:false});
+        }
+    },
+
     
     logout: async () => {
         try {
@@ -46,6 +62,5 @@ export const useAuthStore = create((set)=>({
             toast.error("Error logging out")
             console.log("error in logout in useAuthStore",error.response.data.message)
         }
-    }
-
+    },
 }));
